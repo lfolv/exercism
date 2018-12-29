7ad37ad949e221d334dc69cf796a7f4ce5fbb740
Lucas Furtado de Oliveira <lucas.oliveirasbs@gmail.com>
2018-12-28 22:28:36 -0200

    Change if to switch

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,13 +1,15 @@
 export const toRna = dna =>
   dna.split("").reduce((rna, nucleotide) => {
-    if (nucleotide === "C") {
-      return rna + "G";
-    } else if (nucleotide === "G") {
-      return rna + "C";
-    } else if (nucleotide === "A") {
-      return rna + "U";
-    } else if (nucleotide === "T") {
-      return rna + "A";
+    switch (nucleotide) {
+      case "C":
+        return rna + "G";
+      case "G":
+        return rna + "C";
+      case "A":
+        return rna + "U";
+      case "T":
+        return rna + "A";
+      default:
+        throw new Error("Invalid input DNA.");
     }
-    throw new Error("Invalid input DNA.");
   }, "");
