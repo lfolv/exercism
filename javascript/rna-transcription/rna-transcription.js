2af50d55855300d3c8000c29b4bf5a9bb5eacabb
Lucas Furtado de Oliveira <lucas.oliveirasbs@gmail.com>
2019-01-06 19:28:24 -0200

    Refactory RNA exercise from JS track

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,18 +1,17 @@
+const transcription = {
+  C: "G",
+  G: "C",
+  A: "U",
+  T: "A"
+};
+
 export const toRna = dna =>
   dna
     .split("")
     .map(nucleotide => {
-      switch (nucleotide) {
-        case "C":
-          return "G";
-        case "G":
-          return "C";
-        case "A":
-          return "U";
-        case "T":
-          return "A";
-        default:
-          throw new Error("Invalid input DNA.");
+      if (transcription[nucleotide]) {
+        return transcription[nucleotide];
       }
+      throw new Error("Invalid input DNA.");
     })
     .join("");
