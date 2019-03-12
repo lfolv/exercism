e83b79f35a852f7d361adfa97b346c5d362ce79c
Lucas Furtado de Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-11 21:00:29 -0300

    Init Pangram exercise from JS track

---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,32 @@
+export function isPangram(sentence) {
+  const lowerSentence = sentence.toLowerCase();
+  return alphabet.every(letter => lowerSentence.includes(letter));
+}
+
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "g",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "x",
+  "y",
+  "z"
+];
