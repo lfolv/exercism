5e295e634da534d6048b20d5b0412d6250f61b59
Lucas Furtado de Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-14 07:40:39 -0300

    Remove Scrabble#points method

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,4 +1,33 @@
 class Scrabble
+  POINTS = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  }
+
   def self.score(word)
     Scrabble.new(word).score
   end
@@ -8,7 +37,7 @@ class Scrabble
   end
 
   def score
-    letters.sum { |chr| points(chr) }
+    letters.sum { |chr| POINTS[chr.upcase.to_sym] }
   end
 
   private
@@ -17,36 +46,5 @@ class Scrabble
 
   def letters
     word.scan(/[a-z]/i)
-  end
-
-  def points(chr)
-    {
-      A: 1,
-      B: 3,
-      C: 3,
-      D: 2,
-      E: 1,
-      F: 4,
-      G: 2,
-      H: 4,
-      I: 1,
-      J: 8,
-      K: 5,
-      L: 1,
-      M: 3,
-      N: 1,
-      O: 1,
-      P: 3,
-      Q: 10,
-      R: 1,
-      S: 1,
-      T: 1,
-      U: 1,
-      V: 4,
-      W: 4,
-      X: 8,
-      Y: 4,
-      Z: 10
-    }[chr.upcase.to_sym]
   end
 end
