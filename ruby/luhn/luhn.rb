f46f8dd6203930131d6c776003e709ba5aa3583e
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-15 08:44:28 -0300

    Resolve Luhn Exercise from Ruby track

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,4 +1,21 @@
 class Luhn
   def self.valid?(digits)
+    digits = digits.gsub(' ', '')
+    return false if digits.length <= 1 
+    return false if digits =~ /[^0-9\s]/
+
+    total = 0
+
+    digits.reverse.each_char.with_index do |chr, index|
+      unless index % 2 == 0
+        d = chr.to_i * 2
+        d -= 9 if d > 9
+        total += d
+      else
+        total += chr.to_i
+      end
+    end
+
+    total % 10 == 0
   end
 end
