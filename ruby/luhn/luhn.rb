801f5d0120552d820a6addf19b35cc1d7008fc3b
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-15 09:46:10 -0300

    Rectory Luhn Class

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,4 +1,6 @@
 class Luhn
+  INVALID_CHARACTERS = /[^0-9\s]/
+
   def self.valid?(digits)
     Luhn.new(digits).valid?
   end
@@ -8,8 +10,8 @@ class Luhn
   end
 
   def valid?
-    return false if digits.length <= 1 
-    return false if digits =~ /[^0-9\s]/
+    return false if digits.size <= 1 
+    return false if digits =~ INVALID_CHARACTERS
     sum_digits % 10 == 0
   end
 
@@ -19,17 +21,12 @@ class Luhn
 
   def sum_digits
     digits.reverse.each_char.with_index.sum do |digit, index|
-      double_second_digits(digit, index)
-    end
-  end
-
-  def double_second_digits(digit, index)
-    unless index % 2 == 0
-      d = digit.to_i * 2
-      d -= 9 if d > 9
-      d
-    else
-      digit.to_i
+      if index % 2 != 0
+        value = digit.to_i * 2
+        value > 9 ? value - 9 : value
+      else
+        digit.to_i
+      end
     end
   end
 end
