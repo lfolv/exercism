0adc12001a2a8967135fec17d83ec64ed4c824b2
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-18 10:05:47 -0300

    Create Matrix#column method

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -8,4 +8,18 @@ export class Matrix {
       return line.split(' ').map(value => Number.parseInt(value, 10))
     })
   }
+
+  get columns () {
+    const transpose = []
+    for (let i = 0; i < this.rows[0].length; i++) {
+      transpose.push([])
+    }
+
+    for (let line of this.rows) {
+      for (let i = 0; i < line.length; i++) {
+        transpose[i].push(line[i])
+      }
+    }
+    return transpose
+  }
 }
