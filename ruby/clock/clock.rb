9fae4ab21590dc92ffecb1a455cc0fc3fb5cf440
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-21 08:53:37 -0300

    Refactory Clockl class

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -3,32 +3,37 @@ class Clock
   BASE_MINUTE = 60
   DISPLAY_SIZE = 2
   DISPLAY_COMPLEMENT = '0'
+  MINUTES_IN_A_DAY = 1440
 
   def initialize(hour: 0, minute: 0)
-    @minute = minute % BASE_MINUTE
-    extra_minutes = minute / BASE_MINUTE
-    @hour = (hour + extra_minutes) % BASE_HOUR 
+    @minutes = (minute + (hour * BASE_MINUTE)) % MINUTES_IN_A_DAY
   end
 
   def to_s
-    stringfy_hour = hour.to_s.rjust(DISPLAY_SIZE, DISPLAY_COMPLEMENT)
-    stringfy_minute = minute.to_s.rjust(DISPLAY_SIZE, DISPLAY_COMPLEMENT)
     "#{stringfy_hour}:#{stringfy_minute}"
   end
 
   def +(other)
-    Clock.new(hour: hour + other.hour, minute: minute + other.minute)
+    Clock.new(minute: minutes + other.minutes)
   end
 
   def -(other)
-    Clock.new(hour: hour - other.hour, minute: minute - other.minute)
+    Clock.new(minute: minutes - other.minutes)
   end
 
   def ==(other)
-    hour == other.hour && minute == other.minute
+    minutes == other.minutes
   end
 
   protected
 
-  attr_reader :hour, :minute
+  attr_reader :minutes
+
+  def stringfy_hour
+    ((minutes / BASE_MINUTE) % BASE_HOUR).to_s.rjust(DISPLAY_SIZE, DISPLAY_COMPLEMENT)
+  end
+
+  def stringfy_minute
+    (minutes % BASE_MINUTE).to_s.rjust(DISPLAY_SIZE, DISPLAY_COMPLEMENT)
+  end
 end
