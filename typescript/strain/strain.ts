e5d7950cb577ebd518e88efa494a36c0d8a901bc
Lucas Furtado de Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-24 11:46:09 -0300

    Create Type Filter in Strain

--- typescript/strain/strain.ts
+++ typescript/strain/strain.ts
@@ -1,7 +1,9 @@
-export function keep<T>(array: T[], callback: (e: T) => boolean): T[] {
+type Filter<T> = (e: T) => boolean;
+
+export function keep<T>(array: T[], callback: Filter<T>): T[] {
   return array.filter(callback);
 }
 
-export function discard<T>(array: T[], callback: (e: T) => boolean): T[] {
+export function discard<T>(array: T[], callback: Filter<T>): T[] {
   return array.filter(value => !callback(value));
 }
