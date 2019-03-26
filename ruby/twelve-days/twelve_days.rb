e09687fdd7374549ae633edbeb34f17c17e6244a
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-03-26 16:24:10 -0300

    Extract second part from Twelve Days sentence

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -2,24 +2,27 @@ class TwelveDays
   NUMBER_OF_SENTENCES = 11
   NUMBERS = %w{first second third fourth fifth sixth seventh eighth ninth tenth eleventh twelfth}
   SENTENCES = [
-    'a Partridge in a Pear Tree.',
-    'two Turtle Doves, and a Partridge in a Pear Tree.',
-    'three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.',
-    'twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.'
+    'a Partridge in a Pear Tree',
+    'two Turtle Doves',
+    'three French Hens',
+    'four Calling Birds',
+    'five Gold Rings',
+    'six Geese-a-Laying',
+    'seven Swans-a-Swimming',
+    'eight Maids-a-Milking',
+    'nine Ladies Dancing',
+    'ten Lords-a-Leaping',
+    'eleven Pipers Piping',
+    'twelve Drummers Drumming'
   ]
 
   def self.song
     ''.tap do |result|
       (0..NUMBER_OF_SENTENCES).each do |n|
-        result << "On the #{NUMBERS[n]} day of Christmas my true love gave to me: #{SENTENCES[n]}\n"
+        result << "On the #{NUMBERS[n]} day of Christmas my true love gave to me: "
+        result << SENTENCES[1..n].reverse.join(', ')
+        result << (n.zero? ? SENTENCES[0] : ", and #{SENTENCES[0]}")
+        result << ".\n"
         result << "\n" unless n == NUMBER_OF_SENTENCES
       end
     end
