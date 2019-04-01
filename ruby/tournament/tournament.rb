0b7f45aa246f05931a0461dfa077d0a21e8a42fa
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-01 10:41:39 -0300

    Handle with names in input in Tourmament.tally

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,5 +1,11 @@
 class Tournament
   def self.tally(input)
-    "Team                           | MP |  W |  D |  L |  P\n"
+    t = "Team                           | MP |  W |  D |  L |  P\n"
+    input.split("\n").each do |line|
+      team1, team2, match_result = line.split(";")
+      t << "#{team1}             |  1 |  1 |  0 |  0 |  3\n"
+      t << "#{team2}             |  1 |  0 |  0 |  1 |  0\n"
+    end
+    t
   end
 end
