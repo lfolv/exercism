6a9af2d3dbcf55f131e1f1e7f8b1996168ad5228
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-01 13:52:07 -0300

    Update Tournament.tally to save in a hash

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,10 +1,29 @@
 class Tournament
   def self.tally(input)
     t = "Team                           | MP |  W |  D |  L |  P\n"
+    tournament = Hash.new()
+
     input.split("\n").each do |line|
       team1, team2, match_result = line.split(";")
-      t << "#{team1}             |  1 |  1 |  0 |  0 |  3\n"
-      t << "#{team2}             |  1 |  0 |  0 |  1 |  0\n"
+      tournament[team1] = {
+        mp: 1,
+        w: 1,
+        d: 0,
+        l: 0,
+        p: 3
+      }
+      tournament[team2] = {
+        mp: 1,
+        w: 0,
+        d: 0,
+        l: 1,
+        p: 0
+      }
+    end
+
+
+    tournament.each do |key, values|
+      t << "#{key}             |  %d |  %d |  %d |  %d |  %d\n" % [values[:mp], values[:w], values[:d], values[:l], values[:p]]
     end
     t
   end
