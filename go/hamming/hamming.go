3968e6b6a70e3fb33418996c19d105b2c8c6390f
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-01 08:42:34 -0300

    Refactory Distance

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -6,7 +6,7 @@ import "errors"
 // Distance calcule the Hamming difference between two DNA strands
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
-		return -1, errors.New("lenght of a and b can't be different")
+		return 0, errors.New("lenght of a and b can't be different")
 	}
 	distance := 0
 	for i:= 0; i < len(a); i++ {
