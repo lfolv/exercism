40d5729bcc15dba8e54aa241d17ad1eb25593482
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-01 14:05:43 -0300

    Update Tournament.tally to use Hash default value

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,24 +1,24 @@
 class Tournament
   def self.tally(input)
     t = "Team                           | MP |  W |  D |  L |  P\n"
-    tournament = Hash.new()
+    tournament = Hash.new { |hash, key| hash[key] = {
+        mp: 0,
+        w: 0,
+        d: 0,
+        l: 0,
+        p: 0
+    }}
 
     input.split("\n").each do |line|
       team1, team2, match_result = line.split(";")
-      tournament[team1] = {
-        mp: 1,
-        w: 1,
-        d: 0,
-        l: 0,
-        p: 3
-      }
-      tournament[team2] = {
-        mp: 1,
-        w: 0,
-        d: 0,
-        l: 1,
-        p: 0
-      }
+
+      tournament[team1][:mp] += 1
+      tournament[team1][:w] += 1
+      tournament[team1][:p] += 3
+
+
+      tournament[team2][:mp] += 1
+      tournament[team2][:l] += 1
     end
 
 
