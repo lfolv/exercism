562e2a28646ff1a6c9de53ba15adf1ea3aefe876
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-02 12:21:25 -0300

    Add logic to ELT.transform to handle with multiple values

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,5 +1,11 @@
 class ETL
-  def self.transform(old)
-    { "a" => 1 }
+  def self.transform(old_system)
+    new_system = {}
+    old_system.each do |key, values|
+      values.each do |value|
+        new_system[value.downcase] = key
+      end
+    end
+    new_system
   end
 end
