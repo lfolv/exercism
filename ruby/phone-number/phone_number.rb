4a84647bbe956ab541b19bbace718ceb29f00afa
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-04 09:20:03 -0300

    Resolve Phone Number exercise from Ruby Track

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,6 +1,10 @@
 class PhoneNumber
   def self.clean(number)
+    return nil if number.match(/[a-zA-Z@:!]/)
+
     numbers = number.scan(/\d/)
+    numbers = numbers[1..-1] if numbers.length == 11 && numbers.first == '1'
+    return nil if numbers.first == '0' or numbers.first == '1' or numbers[3] == '0' or numbers[3] == '1'
     return numbers.join('') if numbers.length == 10
   end
 end
