15ec7b1db506fe2e310901c8e0edc3cea9faab09
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-04 09:57:59 -0300

    Add documentation to PhoneNumber class

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,15 +1,40 @@
+# PhoneNumber class represents a North American Numbering Plan (NANP)
+# It is a telephone numbering system used by many countries in North America
+# like the United States, Canada or Bermuda
+#
+# @author Lucas Oliveira
+# @since 0.1.0
 class PhoneNumber
   INVALID_CHARACTERS = /[a-zA-Z@:!]/
   DIGITS = /\d/
 
+  # Clean up differenly formatted telephone numbers by removing punctuation and
+  # contry code (1) if present
+  #
+  # @param [String] number a telephone number
+  # @return [String] the clean number
+  #
+  # @example Clean up a telephone number
+  #   PhoneNumber.clean("(223) 456-7890") #=> ""2234567890""
   def self.clean(number)
     PhoneNumber.new(number).clean
   end
 
+  # Create a PhoneNumber object
+  #
+  # @param [String] number a telephone number
   def initialize(number)
     @number = number
   end
 
+  # Clean up differenly formatted telephone numbers by removing punctuation and
+  # contry code (1) if present
+  #
+  # @return [String] the clean number
+  #
+  # @example Clean up a telephone number
+  #   phone_number = PhoneNumber.new("(223) 456-7890")
+  #   phone_number.clean #=> ""2234567890""
   def clean
     return nil if invalid?
     digits.join('')
