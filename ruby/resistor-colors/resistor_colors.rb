51322b9120e0ccd3b97dcefad1def7a56506cf76
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-08 08:57:19 -0300

    Refactory ResistorColors class

--- ruby/resistor-colors/resistor_colors.rb
+++ ruby/resistor-colors/resistor_colors.rb
@@ -1,20 +1,9 @@
-# Class ResistorColors provide helpful methods to get the value of the resistor
-# bands
+# module ResistorColors provide helpful functions to get the value of the
+# resistor bands
 #
 # @author Lucas Oliveira
-class ResistorColors
-  COLORS = {
-    black: '0',
-    brown: '1',
-    red: '2',
-    orange: '3',
-    yellow: '4',
-    green: '5',
-    blue: '6',
-    violet: '7',
-    grey: '8',
-    white: '9'
-  }
+module ResistorColors
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
 
   # Take two colors as input, and output the correct number
   #
@@ -24,28 +13,9 @@ class ResistorColors
   # @example Get correct value of a band
   #   ResistorColors.value([["brown", "black"]]) #=> 10
   def self.value(colors)
-    ResistorColors.new(colors).value
+    colors
+      .map(&COLORS.method(:index))
+      .join
+      .to_i
   end
-
-  # Create a new ResistorColors object
-  #
-  # @param [Array<String>] :colors The colors of the bands
-  def initialize(colors)
-    @colors = colors
-  end
-
-  # Output the correct number of the ResistorColors object
-  #
-  # @return [Integer] the correct number
-  #
-  # @example Get correct value of a band
-  #   resistor = ResistorColors.new([["brown", "black"]])
-  #   resistor.value #=> 10
-  def value
-    colors.sum("") { |color| COLORS[color.to_sym] }.to_i
-  end
-
-  private
-
-  attr_reader :colors
 end
