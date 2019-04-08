a5208dc0efae7f3ca3966a8098840103d45ca563
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-08 12:28:20 -0300

    Add documentation to Hamming module

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,13 @@
+"""
+hamming module function to calculate the Hamming Distance between two DNA
+strands
+"""
+
+
 def distance(strand_a, strand_b):
+    """
+    Calculate the Hamming Distance between two DNA strands
+    """
     if len(strand_a) != len(strand_b):
         raise ValueError("Invalid strands")
     count = 0
