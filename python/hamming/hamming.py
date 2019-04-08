0ed40a6f39c2697753688f624f68fd403a9d4a95
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-08 10:51:26 -0300

    Resulve Hamming exercise from Python track

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Invalid strands")
+    count = 0
+    for index, value in enumerate(strand_a):
+        if not value == strand_b[index]:
+            count += 1
+    return count
+
