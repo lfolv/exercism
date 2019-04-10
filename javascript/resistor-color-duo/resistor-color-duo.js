78dfed5377f5909e576bee253a442ea4d99b7632
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-10 08:47:42 -0300

    Add Docs to resistor-color-duo module

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,7 @@
+/**
+ * module resistor-color-duo provides helpful function to handle with
+ * Raspiberry PI resistors.
+ */
 const COLORS = [
   "black",
   "brown",
@@ -11,6 +15,15 @@ const COLORS = [
   "white"
 ];
 
+/**
+ * Calculate the value of the band from two resistor colors
+ *
+ * @param {string[]} colors - A list of resistor colors
+ * @returns {number} The value of the band
+ *
+ * @example
+ *  value(["brown", "black"]) #=> 10
+ */
 export function value(colors) {
   return Number.parseInt(
     colors.reduce((result, color) => result + COLORS.indexOf(color), ""),
