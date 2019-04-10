056ae907a0201fb11e6f8d9612370a583b4ee52d
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-10 12:28:13 -0300

    Add Squares#sum_of_squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -12,12 +12,22 @@ class Squares
 
   # Calculate the square of the sum of the first N natural numbers
   #
-  # @return [Int] the square of the sum of the first N natual numbers
+  # @return [Int] square of the sum
   #
   # @example
   #  Squares.new(100).sum_of_squares #=> 25502500
   def square_of_sum
     (1..n).sum ** 2
+  end
+
+  # Calculate the sum of the squares of the first N natural numbers
+  #
+  # @return [Int] sum of the squares
+  #
+  # @example
+  #   Squares.new(100).difference #=> 338350
+  def sum_of_squares
+    (1..n).sum { |current| current ** 2 }
   end
 
   private
