09ceb7cbb5270bb732085f66497cef4c8288c49a
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-11 12:22:47 -0300

    Resolve Isogram exercise from Go track

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,13 +1,17 @@
+// Package isogram provides helpful method to handle with isogram sentences
+// An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, however spaces and
+// hyphens are allowed to appear multiple times
 package isogram
 
 import (
 	"unicode"
 )
 
+// IsIsogram determines if a word or phrase os an isogram
 func IsIsogram(phrase string) bool {
 	letters := make(map[rune]bool)
 
-	for _, letter := range(phrase) {
+	for _, letter := range phrase {
 		if letter == ' ' || letter == '-' {
 			continue
 		}
