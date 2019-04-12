c1f89dbcde183928c225f75ebb777486249a8e99
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-12 08:44:44 -0300

    Add docs to Complement module

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,6 +1,17 @@
+# Provides helpful method to handle with RNA complement
+#
+# @author Lucas Oliveira
+# @since 0.1.0
 module Complement
   COMPLEMENTS = Hash[*%w{G C C G T A A U}]
 
+  # Given a DNA strand, return its RNA complement (per RNA transcription)
+  #
+  # @param [String] dna-strand - A DNA strand
+  # @return [String] the RNA complement
+  #
+  # @example
+  #   Complement.of_dna('ACGTGGTCTTAA') #=> 'UGCACCAGAAUU'
   def self.of_dna(dna_strand)
     dna_strand.each_char.sum('') { |nucleotide| COMPLEMENTS[nucleotide] }
   end
