ec842494600b7a6991f78be7fc77cb9d5c47add7
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-15 12:45:03 -0300

    Update Docs from Squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,9 +1,9 @@
 # class Squares provides helper methods to handle with square numbers
-# calculations
+# calculations.
 #
 # @author Lucas Oliveira
 class Squares
-  # Create a new Squares object
+  # Create a new Squares object.
   #
   # @param [Int] n - first N natual numbers to calculate
   def initialize(number)
@@ -11,7 +11,7 @@ class Squares
   end
 
   # Calculate the difference between the square of sum and the sum of the
-  # squares of the first N natural numbers
+  # squares of the first N natural numbers.
   #
   # @return [Int] the difference of squares
   #
@@ -21,7 +21,9 @@ class Squares
     @difference ||= square_of_sum - sum_of_squares
   end
 
-  # Calculate the square of the sum of the first N natural numbers
+  # Calculate the square of the sum of the first N natural numbers.
+  # The square of the sum of the first ten natural numbers is
+  # (1 + 2 + ... + 10) ** 2 = 55 ** 2 = 3025.
   #
   # @return [Int] square of the sum
   #
@@ -32,11 +34,13 @@ class Squares
   end
 
   # Calculate the sum of the squares of the first N natural numbers
+  # The sum of the squares of the first ten natural numbers is
+  # 1 ** 2 + 2 ** 2 + ... + 10 ** 2 = 385.
   #
   # @return [Int] sum of the squares
   #
   # @example
-  #   Squares.new(100).difference #=> 338350
+  #   Squares.new(100).sum_of_squares #=> 338350
   def sum_of_squares
     (number + 1) * number * (2 * number + 1) / 6
   end
