cff17803c66750c26d7817bef5b418f2356ccb20
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-16 08:48:31 -0300

    All all planets orbital periods to SpaceAge class

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -4,20 +4,26 @@
 class SpaceAge
   ORBITAL_PERIOD = {
     earth: 31557600.0,
-    mercury: 7600543.81992
+    mercury: 7600543.81992,
+    venus: 19414149.052176,
+    mars: 59354032.690079994,
+    jupiter: 374355659.124,
+    saturn: 929292362.8848,
+    uranus: 2651370019.3296,
+    neptune: 5200418560.032001
   }
-
-  ORBITAL_PERIOD.each do |planet, orbital_period|
-    define_method "on_#{planet}" do
-      seconds / orbital_period
-    end
-  end
 
   # Create a new SpaceAge object
   #
   # @param [Int] seconds - Age in seconds
   def initialize(seconds)
     @seconds = seconds
+  end
+
+  ORBITAL_PERIOD.each do |planet, orbital_period|
+    define_method "on_#{planet}" do
+      seconds / orbital_period
+    end
   end
 
   private
