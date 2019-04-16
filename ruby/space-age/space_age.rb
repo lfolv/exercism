c78b403f0bb105f528409cfe72d29bbc537f8a6e
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-16 08:38:55 -0300

    Add docs to SpaceAge

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,17 +1,37 @@
+# Given an age in seconds, calculate how old someone would be on the planet
+#
+# @author Lucas Oliveira
 class SpaceAge
   ORBITAL_PERIOD = {
     earth: 31557600.0,
     mercury: 7600543.81992
   }
 
+  # Create a new SpaceAge object
+  #
+  # @param [Int] seconds - Age in seconds
   def initialize(seconds)
     @seconds = seconds
   end
 
+  # Calculate how old someone would be on earth
+  #
+  # @return [Float] Age on earth
+  #
+  # @example
+  #   age = SpaceAge.new(1_000_000_000)
+  #   age.on_earth #=> 31.69
   def on_earth
     seconds / ORBITAL_PERIOD[:earth]
   end
 
+  # Calculate how old someone would be on mercury
+  #
+  # @return [Float] age on mercury
+  #
+  # @example
+  #   age = SpaceAge.new(2_134_835_688)
+  #   age.on_earth #=> 280.88
   def on_mercury
     seconds / ORBITAL_PERIOD[:mercury]
   end
