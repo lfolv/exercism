3221b2a1252ac4b23a9985c5b13bb7aeb1f4d6b9
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-25 08:45:15 -0300

    Create Triangle#scalene?

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -13,7 +13,18 @@ class Triangle
     sides[0] == sides[1] || sides[0] == sides[2] || sides[1] == sides[2]
   end
 
+  def scalene?
+    return false if inequality?
+    sides[0] != sides[1] && sides[0] != sides[2]
+  end
+
   private
 
   attr_reader :sides
+
+  def inequality?
+    sides[0] + sides[1] < sides[2] ||
+    sides[0] + sides[2] < sides[1] ||
+    sides[1] + sides[2] < sides[0]
+  end
 end
