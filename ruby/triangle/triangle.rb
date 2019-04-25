97e2977a156504e3894270183d22e1b996271f99
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-25 12:40:35 -0300

    Refactory Triagle class

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -4,17 +4,17 @@ class Triangle
   end
 
   def equilateral?
-    return false if inequality? || sides.all?(&:zero?)
+    return false if invalid?
     sides[0] == sides[1] && sides[0] == sides[2]
   end
 
   def isosceles?
-    return false if inequality? || sides.count(1) == 2
+    return false if invalid? || sides.count(1) == 2
     sides[0] == sides[1] || sides[0] == sides[2] || sides[1] == sides[2]
   end
 
   def scalene?
-    return false if inequality?
+    return false if invalid?
     sides[0] != sides[1] && sides[0] != sides[2]
   end
 
@@ -22,9 +22,8 @@ class Triangle
 
   attr_reader :sides
 
-  def inequality?
-    sides[0] + sides[1] < sides[2] ||
-    sides[0] + sides[2] < sides[1] ||
-    sides[1] + sides[2] < sides[0]
+  def invalid?
+    a, b, c = sides.sort
+    a.zero? || b.zero? || c.zero? || a + b < c
   end
 end
