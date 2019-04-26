1010e92218535536b957155fe9241801de70c0fa
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-26 09:25:12 -0300

    Update hey func to respond to foreful questions

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,7 +1,12 @@
 const YELL_PATTERN = /^[A-Z\s!]+$/;
 const QUESTION_PATTERN = /^.+\?$/;
+const FOREFUL_QUESTION_PATTEN = /^[A-Z\s!]+\?$/;
 
 export const hey = message => {
+  if (message.match(FOREFUL_QUESTION_PATTEN)) {
+    return "Calm down, I know what I'm doing!";
+  }
+
   if (message.match(YELL_PATTERN)) {
     return "Whoa, chill out!";
   }
