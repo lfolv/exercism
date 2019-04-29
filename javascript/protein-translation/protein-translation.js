39c49e9c1ec2e3a0e962bf71be6740358ad51aa0
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-04-29 09:27:10 -0300

    Add Codons to Protein mapping

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -1,7 +1,18 @@
 const CODONS_TO_PROTEIN = {
   AUG: "Methionine",
   UUU: "Phenylalanine",
-  UUC: "Phenylalanine"
+  UUC: "Phenylalanine",
+  UUA: "Leucine",
+  UUG: "Leucine",
+  UCU: "Serine",
+  UCC: "Serine",
+  UCA: "Serine",
+  UCG: "Serine",
+  UAU: "Tyrosine",
+  UAC: "Tyrosine",
+  UGU: "Cysteine",
+  UGC: "Cysteine",
+  UGG: "Tryptophan"
 };
 
 const translate = (rna_sequence = "") =>
