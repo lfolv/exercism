6022740147ded52c6704b915a5343840021effcf
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-03 09:51:33 -0300

    Resolve RNA Transcription exercise from TS track

---
+++ typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,29 @@
+const COMPLEMENT: { [key: string]: string } = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
+};
+
+const DIVISOR = "";
+
+class Transcriptor {
+  toRna(dna_strands: string): string {
+    return dna_strands
+      .split(DIVISOR)
+      .map(this.toRnaNucleotide)
+      .join(DIVISOR);
+  }
+
+  toRnaNucleotide(dna_nucleodite: string): string {
+    const rna_nucledite = COMPLEMENT[dna_nucleodite];
+
+    if (!rna_nucledite) {
+      throw new Error("Invalid input DNA.");
+    }
+
+    return rna_nucledite;
+  }
+}
+
+export default Transcriptor;
