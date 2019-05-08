7f056fb50bc9999099e63a19246de8bd28b28bd5
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-08 08:39:36 -0300

    Add rows method to Triangle class

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -2,4 +2,16 @@ export class Triangle {
   constructor(numberOfRows) {
     this.numberOfRows = numberOfRows;
   }
+
+  get rows() {
+    let lines = [];
+    for (let n = 0; n < this.numberOfRows; n++) {
+      let line = [1];
+      for (let k = 0; k < n; k++) {
+        line.push((line[k] * (n - k)) / (k + 1));
+      }
+      lines.push(line);
+    }
+    return lines;
+  }
 }
