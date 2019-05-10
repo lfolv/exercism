43f357ea63e3080a6eb29ec8e58ff87c49c1705c
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-10 09:23:49 -0300

    Add of_codon function to Translation module

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -1,2 +1,25 @@
 module Translation
+  CODONS_TO_AMINO_ACID = {
+    'AUG' => "Methionine",
+    'UUU' => "Phenylalanine",
+    'UUC' => "Phenylalanine",
+    'UUA' => "Leucine",
+    'UUG' => "Leucine",
+    'UCU' => "Serine",
+    'UCC' => "Serine",
+    'UCA' => "Serine",
+    'UCG' => "Serine",
+    'UAU' => "Tyrosine",
+    'UAC' => "Tyrosine",
+    'UGU' => "Cysteine",
+    'UGC' => "Cysteine",
+    'UGG' => "Tryptophan",
+    'UAA' => "STOP",
+    'UAG' => "STOP",
+    'UGA' => "STOP"
+  }
+
+  def self.of_codon(rna_sequence)
+    CODONS_TO_AMINO_ACID[rna_sequence]
+  end
 end
