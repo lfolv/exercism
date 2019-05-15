6c023841fee73af63b5f1884c3ff4f608da9c2ec
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-15 15:49:55 -0300

    Extract multiple? method from SumOfMultiples#to

--- ruby/sum-of-multiples/sum_of_multiples.rb
+++ ruby/sum-of-multiples/sum_of_multiples.rb
@@ -5,11 +5,15 @@ class SumOfMultiples
 
   def to(limit)
     (1...limit)
-      .select { |number| numbers.any? { |divisor| (number % divisor).zero? } }
+      .select { |number| multiple?(number) }
       .sum
   end
 
   private
 
   attr_reader :numbers
+
+  def multiple?(number)
+    numbers.any? { |divisor| (number % divisor).zero? }
+  end
 end
