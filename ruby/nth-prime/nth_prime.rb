a9f71dc7d0c42a1e91b4da681dfd115c3d633a89
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-20 10:51:08 -0300

    Add prime? funtion to Prime module

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -2,4 +2,11 @@ module Prime
   def self.nth(index)
     2
   end
+
+  def self.prime?(number)
+    (2..number).each do |n|
+      return false if number % n == 0
+    end
+    return true
+  end
 end
