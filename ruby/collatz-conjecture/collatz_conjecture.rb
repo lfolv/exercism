2f2d83ce8258a468499919f55de78fe6c525024a
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-21 13:05:20 -0300

    Refactory steps method from CollatzConjecture module

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -2,11 +2,9 @@ module CollatzConjecture
   def self.steps(n)
     raise ArgumentError.new if n <= 0
 
-    step = 0
-    until n == 1
+    (0..Float::INFINITY).each do |step|
+      return step if n == 1
       n = n.even? ? n / 2 : n*3 + 1
-      step += 1
     end
-    step
   end
 end
