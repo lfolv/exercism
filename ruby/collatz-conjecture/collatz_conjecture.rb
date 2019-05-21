9267809fe13105e0d1344371d8f94be5b7e3cf4a
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-21 13:02:59 -0300

    Update steps method from CollatzConjecture module

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -2,11 +2,11 @@ module CollatzConjecture
   def self.steps(n)
     raise ArgumentError.new if n <= 0
 
-    current_step = 0
+    step = 0
     until n == 1
       n = n.even? ? n / 2 : n*3 + 1
-      current_step += 1
+      step += 1
     end
-    current_step
+    step
   end
 end
