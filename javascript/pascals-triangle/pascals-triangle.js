249c5823229761ffcf8abe7e3c7ac7432881d001
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-22 12:19:21 -0300

    Extract getLine function from Triangle class
    Use scope over convertion to turn method to private.

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -6,20 +6,20 @@ export class Triangle {
   get rows() {
     let lines = [];
     for (let n = 0; n < this.numberOfRows; n++) {
-      lines.push(this._getLine(n));
+      lines.push(getLine(n));
     }
     return lines;
   }
 
   get lastRow() {
-    return this._getLine(this.numberOfRows - 1);
+    return getLine(this.numberOfRows - 1);
   }
+}
 
-  _getLine(n) {
-    let line = [1];
-    for (let k = 0; k < n; k++) {
-      line.push((line[k] * (n - k)) / (k + 1));
-    }
-    return line;
+const getLine = (n) => {
+  let line = [1];
+  for (let k = 0; k < n; k++) {
+    line.push((line[k] * (n - k)) / (k + 1));
   }
+  return line;
 }
