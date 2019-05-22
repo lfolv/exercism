390aecd205782045e84ba476cbb3cb12e443eba2
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-22 14:43:57 -0300

    Add push method to LinkedList class

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,3 +1,22 @@
 export class LinkedList {
+  constructor() {
+    this.first = null
+    this.last = null
+  }
 
+  push(value) {
+    const node = createNode({value, previos: this.last})
+    if (!this.first) {
+      this.first = node
+    } else {
+      this.last.next = node
+    }
+    this.last = node
+  }
 }
+
+const createNode = ({ value = null, previos = null, next = null}) => ({
+  value,
+  previos,
+  next
+})
