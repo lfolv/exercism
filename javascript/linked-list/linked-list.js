4f1a09950f7b738d01f4aebdc9d4fa1b3f1be1e6
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-05-23 12:45:44 -0300

    Update pop method from LinkedList class
    Fix bug when have just one element

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -26,6 +26,9 @@ export class LinkedList {
 
   pop() {
     const { value } = this.tail
+    if (this.head == this.tail) {
+      this.head = null
+    }
     this.tail = this.tail.previos
     return value
   }
--- javascript/linked-list/linked-list.spec.js
+++ javascript/linked-list/linked-list.spec.js
@@ -50,7 +50,7 @@ describe('LinkedList', () => {
     list.push(20);
     expect(list.count()).toBe(2);
   });
-  xtest('sets head/tail after popping last element', () => {
+  test('sets head/tail after popping last element', () => {
     const list = new LinkedList();
     list.push(10);
     list.pop();
