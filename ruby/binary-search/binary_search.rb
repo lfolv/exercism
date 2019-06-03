90f3316d7685e134a85e3082eedb6eb82539a83d
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-03 09:22:11 -0300

    Extract valid? method from BinarySearch#initialize

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -2,7 +2,14 @@ class BinarySearch
   attr_reader :list
 
   def initialize(list)
-    list.each_cons(2) { |a| raise ArgumentError if  a[0] >= a[1] }
     @list = list
+    raise ArgumentError unless valid?
+  end
+
+  private
+
+  def valid?
+    list.each_cons(2) { |a| return false if a[0] >= a[1] }
+    true
   end
 end
