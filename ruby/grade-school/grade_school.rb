dcda1a0d785e889d3de9ee595c3669b3529b848d
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-04 15:48:25 -0300

    Create add method to School class

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,5 +1,18 @@
 class School
-  def students(grade)
-    []
+
+  def initialize
+    @grades = Hash.new([])
   end
+
+  def students(grade)
+    grades[grade]
+  end
+
+  def add(name, grade)
+    grades[grade] << name
+  end
+
+  private
+
+  attr_reader :grades
 end
