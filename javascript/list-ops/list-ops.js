a8fb796bb5237311fdeade3c6becf4b2b1baf8ae
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-12 14:41:28 -0300

    Create List class with append method

---
+++ javascript/list-ops/list-ops.js
@@ -0,0 +1,29 @@
+export class List {
+  constructor() {
+  }
+
+  append() {
+    return new List()
+  }
+
+  concat() {
+  }
+
+  filter() {
+  }
+
+  map() {
+  }
+
+  length() {
+  }
+
+  foldl() {
+  }
+
+  foldr() {
+  }
+
+  reverse() {
+  }
+}
