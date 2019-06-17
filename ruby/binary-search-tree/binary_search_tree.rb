3725de2b348aa5fe448a652eec4bfa0f115c9314
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-17 14:09:09 -0300

    Extract inserts method from Bst#insert method

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -7,21 +7,29 @@ class Bst
 
   def insert(data, current = self)
     if data <= current.data
-      if current.left.nil?
-        current.left = Bst.new(data)
-      else
-        insert(data, current.left)
-      end
+      insert_left(data, current)
     else
-      if current.right.nil?
-        current.right = Bst.new(data)
-      else
-        insert(data, current.right)
-      end
+      insert_right(data, current)
     end
   end
 
   protected
 
   attr_writer :left, :right
+
+  def insert_left(data, current)
+    if current.left.nil?
+      current.left = Bst.new(data)
+    else
+      insert(data, current.left)
+    end
+  end
+
+  def insert_right(data, current)
+    if current.right.nil?
+      current.right = Bst.new(data)
+    else
+      insert(data, current.right)
+    end
+  end
 end
