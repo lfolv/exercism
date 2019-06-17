99bc27b1310f2867e1d9d1b62662fa6bb1b5b1d2
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-17 12:52:04 -0300

    Add insert method to Bst class

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -1,7 +1,15 @@
 class Bst
-  attr_reader :data
+  attr_reader :data, :left
 
   def initialize(data)
     @data = data
   end
+
+  def insert(data)
+    self.left = Bst.new(data)
+  end
+
+  private
+
+  attr_writer :left
 end
