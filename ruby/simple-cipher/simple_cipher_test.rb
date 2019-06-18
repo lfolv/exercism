2410fd27b3200de315f9c9034ca704e6e08a7b90
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-18 14:25:31 -0300

    Add test coverage to Cipher class

--- ruby/simple-cipher/simple_cipher_test.rb
+++ ruby/simple-cipher/simple_cipher_test.rb
@@ -87,7 +87,6 @@ class SubstitutionCipherTest < Minitest::Test
   end
 
   def test_cipher_encode_wrap
-    skip
     plaintext = 'zzzzzzzzzz'
     ciphertext = 'zabcdefghi'
     assert_equal(ciphertext, @cipher.encode(plaintext))
