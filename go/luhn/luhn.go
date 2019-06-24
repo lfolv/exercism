f9580e1a44dd57de9ed273e31e2a17d9fd79dbd7
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-06-24 09:02:58 -0300

    Update Valid function in luhn package

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -2,27 +2,28 @@ package luhn
 
 import (
 	"strconv"
+	"strings"
 )
 
 func Valid(input string) bool {
 	sum := 0
-	offset := 1
-	for i:= len(input) - 1; i >= 0; i-- {
-		j := len(input) - i - offset
-		rune := input[i]
-		if rune == ' ' {
-			offset++
-		} else {
-			n, _ := strconv.Atoi(string(rune))
-			if j % 2 != 0 {
-				n = n * 2
-				if n > 9 {
-					n = n - 9
-				}
-			}
-			sum += n
+	input = strings.Replace(input, " ", "", -1)
 
+	if len(input) <= 1 {
+		return false
+	}
+
+	for i:= len(input) - 1; i >= 0; i-- {
+		j := len(input) - i
+		rune := input[i]
+		n, _ := strconv.Atoi(string(rune))
+		if j % 2 != 0 {
+			n = n * 2
+			if n > 9 {
+				n = n - 9
+			}
 		}
+		sum += n
 	}
 	return sum % 10 == 0
 }
