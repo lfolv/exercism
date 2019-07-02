fe651cdd547a37e3eb2a1b4e77ca7485e3bc43a1
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-02 09:46:50 -0300

    Update isIsogram method from isogram package

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,10 +1,8 @@
-const TO_CHECK_CHRS = /[^\s\-]/
-
 export const isIsogram = (sentence) => {
   const letters = {}
 
   for (const letter of sentence.toUpperCase()) {
-    if (letter.match(TO_CHECK_CHRS) && letters[letter]) {
+    if (letter !== ' ' && letter !== '-' && letters[letter]) {
       return false
     }
 
