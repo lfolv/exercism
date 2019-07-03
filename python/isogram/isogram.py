729d626d5c7fadbae42b6f8ed4e2fed2f505c341
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-03 08:37:35 -0300

    Update is_isogram method from isogram module

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,5 @@
+
 def is_isogram(string):
   chrs = string.upper().replace(' ', '').replace('-', '')
-  return all(chrs.index(chr) == index for index, chr in enumerate(chrs))
+  used_chrs = set(chrs)
+  return len(chrs) == len(used_chrs)
