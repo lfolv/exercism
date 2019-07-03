482829d426bfea399fa26b03496fbf4bbb5ea9aa
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-03 08:33:06 -0300

    Remove unnecessary import in isogram package

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,3 @@
-from string import ascii_uppercase
-
 def is_isogram(string):
   chrs = string.upper().replace(' ', '').replace('-', '')
   return all(chrs.index(chr) == index for index, chr in enumerate(chrs))
