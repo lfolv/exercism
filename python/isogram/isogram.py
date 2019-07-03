36d1783a7e7b0263476a7901b2c2e08488086214
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-03 08:38:16 -0300

    Refactory is_isogram method from isogram package

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,5 @@
 
 def is_isogram(string):
   chrs = string.upper().replace(' ', '').replace('-', '')
-  used_chrs = set(chrs)
-  return len(chrs) == len(used_chrs)
+  uniq_chrs = set(chrs)
+  return len(chrs) == len(uniq_chrs)
