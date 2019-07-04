1500808abc61910e4f42615636c817bda19797fc
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-04 09:34:56 -0300

    Refactory count method from Words class
    Extract reducer function.

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,14 +1,14 @@
+const reducer = (counter, word) => ({
+  ...counter,
+  [word]: Number.isInteger(counter[word]) ? counter[word] + 1 : 1
+})
+
 export class Words {
   count(phrase) {
     return phrase
       .trim()
       .toLowerCase()
       .split(/\s+/)
-      .reduce((counter, word) => ({
-        ...counter,
-        [word]: Number.isInteger(counter[word]) ?
-          counter[word] + 1 :
-          1
-      }), {})
+      .reduce(reducer, {})
   }
 }
