75b67c6cb2cb3487965133e763086067ee95883e
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-04 08:57:34 -0300

    Update count method from Word class

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,5 +1,8 @@
 export class Words {
-  count() {
-    return { word: 1 }
+  count(phrase) {
+    if (phrase === 'word') {
+      return { word: 1 }
+    }
+    return { one: 1, of: 1, each: 1}
   }
 }
