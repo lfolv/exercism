57891321d128d124ef6a263459e3414d48940405
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-04 09:10:38 -0300

    Add test coverage to Word#count method
    Include numbers to phrase.

--- javascript/word-count/word-count.spec.js
+++ javascript/word-count/word-count.spec.js
@@ -27,7 +27,7 @@ describe('words()', () => {
     expect(words.count('car : carpet as java : javascript!!&@$%^&')).toEqual(expectedCounts);
   });
 
-  xtest('includes numbers', () => {
+  test('includes numbers', () => {
     const expectedCounts = { testing: 2, 1: 1, 2: 1 };
     expect(words.count('testing 1 2 testing')).toEqual(expectedCounts);
   });
