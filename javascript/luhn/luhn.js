2ccdd0235c824ab8b3a532a6c95fdad4ebf80895
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-05 14:18:01 -0300

    Update valid method from Luhn class

--- javascript/luhn/luhn.js
+++ javascript/luhn/luhn.js
@@ -1,9 +1,29 @@
 export class Luhn {
   constructor(number) {
-    this.number = number
+    this._number = number.replace(/\s/g, '');
   }
 
   get valid() {
-    return false
+    if (this._number.length === 1) {
+      return false;
+    }
+
+    let luhnSum = 0
+    const digits = this.number.split('')
+
+    for (let i = 0; i < digits.length; i++) {
+      let d = Number.parseInt(digits[i], 10)
+      const luhnIndex = digits.length - i - 1
+      if (luhnIndex % 2 !== 0) {
+        d = d * 2
+        if (d > 9) {
+          d = d - 9
+        }
+      }
+      luhnSum += d
+    }
+
+    return luhnSum % 10 === 0
   }
 }
+
--- javascript/luhn/luhn.spec.js
+++ javascript/luhn/luhn.spec.js
@@ -11,7 +11,7 @@ describe('Luhn', () => {
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('a simple valid SIN that remains valid if reversed', () => {
+  test('a simple valid SIN that remains valid if reversed', () => {
     const luhn = new Luhn('059');
     expect(luhn.valid).toEqual(true);
   });
@@ -46,37 +46,37 @@ describe('Luhn', () => {
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('valid strings with a non-digit added at the end invalid', () => {
+  test('valid strings with a non-digit added at the end invalid', () => {
     const luhn = new Luhn('059a');
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('valid strings with punctuation included become invalid', () => {
+  test('valid strings with punctuation included become invalid', () => {
     const luhn = new Luhn('055-444-285');
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('valid strings with symbols included become invalid', () => {
+  test('valid strings with symbols included become invalid', () => {
     const luhn = new Luhn('055£ 444$ 285');
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('single zero with space is invalid', () => {
+  test('single zero with space is invalid', () => {
     const luhn = new Luhn(' 0');
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('more than a single zero is valid', () => {
+  test('more than a single zero is valid', () => {
     const luhn = new Luhn('0000 0');
     expect(luhn.valid).toEqual(true);
   });
 
-  xtest('input digit 9 is correctly converted to output digit 9', () => {
+  test('input digit 9 is correctly converted to output digit 9', () => {
     const luhn = new Luhn('091');
     expect(luhn.valid).toEqual(true);
   });
 
-  xtest('strings with non-digits is invalid', () => {
+  test('strings with non-digits is invalid', () => {
     const luhn = new Luhn(':9');
     expect(luhn.valid).toEqual(false);
   });
