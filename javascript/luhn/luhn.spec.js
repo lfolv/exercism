04add615a009adbc92cb63a64e67fcf9383a85f9
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-05 14:00:05 -0300

    Add test to valid method from Luhn class

--- javascript/luhn/luhn.spec.js
+++ javascript/luhn/luhn.spec.js
@@ -6,7 +6,7 @@ describe('Luhn', () => {
     expect(luhn.valid).toEqual(false);
   });
 
-  xtest('a single zero is invalid', () => {
+  test('a single zero is invalid', () => {
     const luhn = new Luhn('0');
     expect(luhn.valid).toEqual(false);
   });
