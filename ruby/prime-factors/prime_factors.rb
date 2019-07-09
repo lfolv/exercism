450a0fc00058e7d6cd7c9b34f9ee14d65bd54476
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-09 08:43:33 -0300

    Update for method from PrimeFactors module

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,5 +1,15 @@
 module PrimeFactors
   def self.for(number)
-    []
+    [].tap do |factors|
+      while number > 1
+        factor = get_minimal_factor_for number
+        number /= factor
+        factors << factor
+      end
+    end
+  end
+
+  def self.get_minimal_factor_for(number)
+    (2..).find { |factor| (number % factor).zero? }
   end
 end
