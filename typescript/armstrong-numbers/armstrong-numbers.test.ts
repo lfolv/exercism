15d5082c76aee59f0b08efd78a0dd9b845c58d0b
Lucas <lucas.oliveirasbs@gmail.com>
2019-07-17 09:06:57 -0300

    Update isArmstrongNumber method from armstrong-number module

--- typescript/armstrong-numbers/armstrong-numbers.test.ts
+++ typescript/armstrong-numbers/armstrong-numbers.test.ts
@@ -9,27 +9,27 @@ describe("Armstrong Numbers", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(10)).toBeFalsy();
   });
 
-  xit("Three digit number that is an Armstrong number", () => {
+  it("Three digit number that is an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(153)).toBeTruthy();
   });
 
-  xit("Three digit number that is not an Armstrong number", () => {
+  it("Three digit number that is not an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(100)).toBeFalsy();
   });
 
-  xit("Four digit number that is an Armstrong number", () => {
+  it("Four digit number that is an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(9474)).toBeTruthy();
   });
 
-  xit("Four digit number that is not an Armstrong number", () => {
+  it("Four digit number that is not an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(9475)).toBeFalsy();
   });
 
-  xit("Seven digit number that is an Armstrong number", () => {
+  it("Seven digit number that is an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(9926315)).toBeTruthy();
   });
 
-  xit("Seven digit number that is not an Armstrong number", () => {
+  it("Seven digit number that is not an Armstrong number", () => {
     expect(ArmstrongNumbers.isArmstrongNumber(9926314)).toBeFalsy();
   });
 });
--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,5 +1,9 @@
 function isArmstrongNumber(n: number): boolean {
-  return n === 5
+  const digits = n.toString().split('').map((n) => Number.parseInt(n, 10))
+  const sum = digits.reduce((prev: number, curr: number, _, ds: number[]): number => {
+    return prev + (curr ** ds.length)
+  }, 0)
+  return n === sum
 }
 
 export default {
