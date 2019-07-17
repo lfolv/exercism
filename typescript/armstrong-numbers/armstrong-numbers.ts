dd778414375d0f06fbc6774828909069ba629074
Lucas <lucas.oliveirasbs@gmail.com>
2019-07-17 09:22:36 -0300

    Extract method from isArmstrongNumber in armstrong-number module
    Refactory isArmsstringNumbe method to have just one responsability.

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,9 +1,12 @@
 function isArmstrongNumber(n: number): boolean {
+  return n === armstrongNumberSum(n)
+}
+
+function armstrongNumberSum(n: number): number {
   const digits = n.toString().split('').map((n) => Number.parseInt(n, 10))
-  const sum = digits.reduce((prev: number, curr: number, _, ds: number[]): number => {
+  return digits.reduce((prev: number, curr: number, _, ds: number[]): number => {
     return prev + (curr ** ds.length)
   }, 0)
-  return n === sum
 }
 
 export default {
