98ef22722e4707417e825e73b4bd86b90400a115
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-07-31 09:10:41 -0300

    Replace Orbital Period to enum

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,12 +1,12 @@
-const PLANET_TO_ORBITAL_PERIOD: { [key: string]: number } = {
-  earth: 31557600,
-  mercury: 7600543.81992,
-  venus: 19414149.052176,
-  mars: 59354032.690079994,
-  jupiter: 374355659.124,
-  saturn: 929292362.8848,
-  uranus: 2651370019.3296,
-  neptune: 5200418560.032001
+enum OrbitalPeriod {
+  earth = 31557600,
+  mercury = 7600543.81992,
+  venus = 19414149.052176,
+  mars = 59354032.690079994,
+  jupiter = 374355659.124,
+  saturn = 929292362.8848,
+  uranus = 2651370019.3296,
+  neptune = 5200418560.032001
 };
 
 class SpaceAge {
@@ -17,39 +17,39 @@ class SpaceAge {
   }
 
   onEarth(): number {
-    return this.age('earth');
+    return this.age(OrbitalPeriod.earth);
   }
 
   onMercury(): number {
-    return this.age('mercury');
+    return this.age(OrbitalPeriod.mercury);
   }
 
   onVenus(): number {
-    return this.age('venus');
+    return this.age(OrbitalPeriod.venus);
   }
 
   onMars(): number {
-    return this.age('mars');
+    return this.age(OrbitalPeriod.mars);
   }
 
   onJupiter(): number {
-    return this.age('jupiter');
+    return this.age(OrbitalPeriod.jupiter);
   }
 
   onSaturn(): number {
-    return this.age('saturn');
+    return this.age(OrbitalPeriod.saturn);
   }
 
   onUranus(): number {
-    return this.age('uranus');
+    return this.age(OrbitalPeriod.uranus);
   }
 
   onNeptune(): number {
-    return this.age('neptune');
+    return this.age(OrbitalPeriod.neptune);
   }
 
-  private age(planet: string): number {
-    return Number((this.seconds / PLANET_TO_ORBITAL_PERIOD[planet]).toFixed(2));
+  private age(orbitalPeriod: OrbitalPeriod): number {
+    return Number((this.seconds / orbitalPeriod).toFixed(2));
   }
 }
 
