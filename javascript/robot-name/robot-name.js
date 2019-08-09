4a4d63e4bd920fa0cd6ba5e64ac6c4160a7a8c30
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-08-09 08:37:49 -0300

    Refactory Robot class
    Typescript provides static methods.

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,35 @@
 const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 export class Robot { 
+  static releaseNames() { 
+    Robot.usedNames = new Set()
+  };
+
+  static newName() {
+    let newName = Robot.generateRobotName()
+
+    while (Robot.usedNames.has(newName)) {
+      newName = Robot.generateRobotName()
+    }
+
+    Robot.usedNames.add(newName)
+    return newName
+  }
+
+  static generateRobotName() {
+    let newName = ''
+
+    for (let i = 0; i < 2; i++) {
+      newName += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length))
+    }
+
+    for (let i = 0; i < 3; i++) {
+      newName += Math.floor(Math.random() * 10)
+    }
+
+    return newName
+  }
+
   constructor() {
     this.reset()
   }
@@ -10,33 +39,8 @@ export class Robot {
   }
 
   reset() {
-    let newName = generateRobotName()
-
-    while (Robot.usedNames.has(newName)) {
-      newName = generateRobotName()
-    }
-
-    Robot.usedNames.add(newName)
-    this._name = newName
+    this._name = Robot.newName()
   }
 }
 
-function generateRobotName() {
-  let newName = ''
-
-  for (let i = 0; i < 2; i++) {
-    newName += CHARACTERS.charAt(Math.floor(Math.random() * CHARACTERS.length))
-  }
-
-  for (let i = 0; i < 3; i++) {
-    newName += Math.floor(Math.random() * 10)
-  }
-
-  return newName
-}
-
-Robot.usedNames = new Set()
-
-Robot.releaseNames = () => { 
-  Robot.usedNames = new Set()
-};
+Robot.releaseNames()
