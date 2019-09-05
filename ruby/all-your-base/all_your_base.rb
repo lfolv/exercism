0e2bfc9ed11cfe32e425a91380c1f3b42b8fea53
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-09-05 09:14:17 -0300

    fixup! Create BaseConverter module with convert function

--- ruby/all-your-base/all_your_base.rb
+++ ruby/all-your-base/all_your_base.rb
@@ -1,5 +1,11 @@
 module BaseConverter
   def self.convert(input_base, digits, output_base)
-    [1]
+    base10 = 0
+    
+    digits.reverse.each.with_index do |digit, index|
+      base10 += digit * (input_base ** index)
+    end
+
+    base10.to_s.split('').map(&:to_i)
   end
 end
