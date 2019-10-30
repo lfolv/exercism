a553f57685597b836e0b0b7d00a10748f852e0dd
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-10-30 09:26:19 -0300

    Extract convertToBase10 function from convert

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -1,10 +1,11 @@
-export const convert = (digits, base_origin, base_to_convert) => {
-  return digits
-    .reverse()
-    .reduce((sum, digit, index) => {
-      return sum + (digit * (base_origin ** index))  
-    }, 0)
+export const convert = (digits, baseOrigin, base_to_convert) => {
+  return convertToBase10(digits, baseOrigin)
     .toString()
-    .split('')
-    .map(digit => Number(digit))
+    .split("")
+    .map(digit => Number(digit));
 };
+
+const convertToBase10 = (digits, baseOrigin) =>
+  digits
+    .reverse()
+    .reduce((sum, digit, index) => sum + digit * baseOrigin ** index, 0);
