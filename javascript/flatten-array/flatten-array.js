92aeb0218023926145d895d6db64a22a51fafc03
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-11-05 15:48:45 -0300

    Update flatten method to ignore undefined

--- javascript/flatten-array/flatten-array.js
+++ javascript/flatten-array/flatten-array.js
@@ -1,3 +1,13 @@
-export const flatten = () => {
-  return []
-}
+export const flatten = list => {
+  let flattedList = [];
+
+  for (const value of list) {
+    if (Array.isArray(value)) {
+      flattedList = [...flattedList, ...flatten(value)];
+    } else if (value !== undefined) {
+      flattedList = [...flattedList, value];
+    }
+  }
+
+  return flattedList;
+};
--- javascript/flatten-array/flatten-array.spec.js
+++ javascript/flatten-array/flatten-array.spec.js
@@ -1,44 +1,77 @@
-import { flatten } from './flatten-array.js';
+import { flatten } from "./flatten-array.js";
 
-describe('FlattenArray', () => {
-  test('flattens a nested list', () => {
+describe("FlattenArray", () => {
+  test("flattens a nested list", () => {
     expect(flatten([[]])).toEqual([]);
   });
 
-  xtest('undefined values are omitted from the final result', () => {
+  test("undefined values are omitted from the final result", () => {
     expect(flatten([1, 2, undefined])).toEqual([1, 2]);
   });
 
-  xtest('null values are omitted from the final result', () => {
+  xtest("null values are omitted from the final result", () => {
     expect(flatten([1, 2, null])).toEqual([1, 2]);
   });
 
-  xtest('flattens a 2 level nested list', () => {
+  xtest("flattens a 2 level nested list", () => {
     expect(flatten([1, [2, 3, 4], 5])).toEqual([1, 2, 3, 4, 5]);
   });
 
-  xtest('flattens a  3 level nested list', () => {
-    expect(flatten([1, [2, 3, 4], 5, [6, [7, 8]]]))
-      .toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  xtest("flattens a  3 level nested list", () => {
+    expect(flatten([1, [2, 3, 4], 5, [6, [7, 8]]])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8
+    ]);
   });
 
-  xtest('flattens a 5 level nested list', () => {
-    expect(flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2]))
-      .toEqual([0, 2, 2, 3, 8, 100, 4, 50, -2]);
+  xtest("flattens a 5 level nested list", () => {
+    expect(flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2])).toEqual([
+      0,
+      2,
+      2,
+      3,
+      8,
+      100,
+      4,
+      50,
+      -2
+    ]);
   });
 
-  xtest('flattens a 6 level nested list', () => {
-    expect(flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8]))
-      .toEqual([1, 2, 3, 4, 5, 6, 7, 8]);
+  xtest("flattens a 6 level nested list", () => {
+    expect(flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8])).toEqual([
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8
+    ]);
   });
 
-  xtest('flattens a 6 level nested list with null values', () => {
-    expect(flatten([0, 2, [[2, 3], 8, [[100]], null, [[null]]], -2]))
-      .toEqual([0, 2, 2, 3, 8, 100, -2]);
+  xtest("flattens a 6 level nested list with null values", () => {
+    expect(flatten([0, 2, [[2, 3], 8, [[100]], null, [[null]]], -2])).toEqual([
+      0,
+      2,
+      2,
+      3,
+      8,
+      100,
+      -2
+    ]);
   });
 
-  xtest('returns an empty list if all values in nested list are null', () => {
-    expect(flatten([null, [[[null]]], null, null, [[null, null], null], null]))
-      .toEqual([]);
+  xtest("returns an empty list if all values in nested list are null", () => {
+    expect(
+      flatten([null, [[[null]]], null, null, [[null, null], null], null])
+    ).toEqual([]);
   });
 });
