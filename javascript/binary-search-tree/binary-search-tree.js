491fc730718618b345fd7ac2a981053d707a2500
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-12-03 11:40:09 -0300

    Create BinarySearchTree class

---
+++ javascript/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,21 @@
+export class BinarySearchTree {
+  constructor(data) {
+    this._data = data;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  get right() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  get left() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  insert() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
