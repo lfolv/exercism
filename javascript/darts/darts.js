fe8f489d9e093d09582b9fd576556f383b905c53
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-12-05 10:00:19 -0300

    Extract constants from darts module
    Avoid magic values is a good choice, is more easy do read and understand
    to code.

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -1,22 +1,29 @@
+const CENTER = [0, 0];
+const INNER_CIRCLE_RADIUS = 1;
+const INNER_CIRCLE_POINTS = 10;
+const MIDDLE_CIRCLE_RADIUS = 5;
+const MIDDLE_CIRCLE_POINTS = 5;
+const OUTER_CIRCLE_RADIUS = 10;
+const OUTER_CIRCLE_POINTS = 1;
+const OUTSIDE_CIRCLE_POINTS = 0;
+
 export const solve = (x, y) => {
-  const distance = calculeDistance([x, y], [0, 0]);
+  const distance = calculeDistance([x, y], CENTER);
 
-  if (distance <= 1) {
-    return 10
-  } 
-
-  if (distance <= 5) {
-    return 5;
+  if (distance <= INNER_CIRCLE_RADIUS) {
+    return INNER_CIRCLE_POINTS;
   }
 
-  if (distance <= 10) {
-    return 1;
+  if (distance <= MIDDLE_CIRCLE_RADIUS) {
+    return MIDDLE_CIRCLE_POINTS;
   }
 
-  return 0;
+  if (distance <= OUTER_CIRCLE_RADIUS) {
+    return OUTER_CIRCLE_POINTS;
+  }
+
+  return OUTSIDE_CIRCLE_POINTS;
 };
 
-const calculeDistance = ([x1, y1], [x2, y2]) => 
-  Math.sqrt((x1 ** 2 + x2 ** 2) + (y1 ** 2 + y2 ** 2))
-
-
+const calculeDistance = ([x1, y1], [x2, y2]) =>
+  Math.sqrt(x1 ** 2 + x2 ** 2 + (y1 ** 2 + y2 ** 2));
