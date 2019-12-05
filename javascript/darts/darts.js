0257881b496242bd16b13d538b66dce0a51e5a31
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-12-05 10:05:35 -0300

    Refactory calculateDistance function
    Use Math.pow function over operator '**' to be more clear what this calc
    do.

--- javascript/darts/darts.js
+++ javascript/darts/darts.js
@@ -26,4 +26,6 @@ export const solve = (x, y) => {
 };
 
 const calculeDistance = ([x1, y1], [x2, y2]) =>
-  Math.sqrt(x1 ** 2 + x2 ** 2 + (y1 ** 2 + y2 ** 2));
+  Math.sqrt(
+    Math.pow(x1, 2) + Math.pow(x2, 2) + (Math.pow(y1, 2) + Math.pow(y2, 2))
+  );
