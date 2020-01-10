15325765d81a1ff97069316302f0d46d7398a5fe
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-09 21:56:45 -0300

    Update sum of multiples to pass in all tests

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,8 @@
 def sum_of_multiples(limit, multiples):
-  return 0
+  sum = 0
+
+  for i in range(1, limit):
+    if any(divisor != 0 and i % divisor == 0 for divisor in multiples):
+      sum += i
+  
+  return sum
