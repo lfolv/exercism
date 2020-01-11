67631346ba2e9996b7bd86533a871315183f3ce3
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-10 21:11:28 -0300

    Use for inline with if startement in sum_of_multiples
    A if in the for loop inline is more easy to read and more pythonic.

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,8 +1,9 @@
 def sum_of_multiples(limit, multiples):
-  sum = 0
+    sum = 0
 
-  for i in range(1, limit):
-    if any(divisor != 0 and i % divisor == 0 for divisor in multiples):
-      sum += i
-  
-  return sum
+    for i in range(1, limit):
+        if any(i % divisor == 0 for divisor in multiples if not divisor == 0):
+            sum += i
+
+    return sum
+
