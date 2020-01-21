ccaaea80c52c056f2b487ae4ebdc1dc0c383ecee
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-21 16:33:21 -0300

    Add decode method to run-lenght-encoding module

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -5,6 +5,13 @@ const shrink = characters =>
     ? characters[0]
     : `${characters.length}${characters[0]}`;
 
-export const decode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const decode = data => {
+  return data
+    .replace(/(\d*)(.)/g, match => {
+      if (match.length === 1) {
+        return match
+      }
+
+      return match[match.length - 1].repeat(Number.parseInt(match))
+    })
 };
