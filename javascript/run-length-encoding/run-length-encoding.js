efc3a6adec0c2acc1aa809ddb99298142f3e1e87
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-21 11:24:59 -0300

    Extract shrink() from encode()

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,12 +1,9 @@
-export const encode = data => {
-  return data.replace(/((.)\2*)/g, match => {
-    if (match.length === 1) {
-      return match[0];
-    }
+export const encode = data => data.replace(/((.)\2*)/g, shrink);
 
-    return `${match.length}${match[0]}`;
-  });
-};
+const shrink = characters =>
+  characters.length === 1
+    ? characters[0]
+    : `${characters.length}${characters[0]}`;
 
 export const decode = () => {
   throw new Error("Remove this statement and implement this function");
