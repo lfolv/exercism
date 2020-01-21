a92e11ea67a02851e0d78df2841b9fc7f3be1277
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-21 10:51:23 -0300

    Update encode() to handle with string with no single characters

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,5 +1,11 @@
 export const encode = data => {
-  return data;
+  return data.replace(/((.)\2*)/g, match => {
+    if (match.length === 1) {
+      return match[0];
+    }
+
+    return `${match.length}${match[0]}`;
+  });
 };
 
 export const decode = () => {
