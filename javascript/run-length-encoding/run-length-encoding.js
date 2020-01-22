e2cd71a16741651e6fbc50ab43ed6412dfe91e27
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-22 08:56:21 -0300

    Extract Regex patterns to constants

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,11 +1,14 @@
-export const encode = data => data.replace(/((.)\2*)/g, shrink);
+const CONSECUTIVE_CHARACTERS = /((.)\2*)/g
+const ENCODED_DATA = /(\d*)(.)/g
+
+export const encode = data => data.replace(CONSECUTIVE_CHARACTERS, shrink);
 
 const shrink = characters =>
   characters.length === 1
     ? characters[0]
     : `${characters.length}${characters[0]}`;
 
-export const decode = data => data.replace(/(\d*)(.)/g, grow);
+export const decode = data => data.replace(ENCODED_DATA, grow);
 
 const grow = encoded =>
   encoded.length === 1
