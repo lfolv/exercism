7d3478bc631913afe71915edbd40a1131544dc5f
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-24 14:01:43 -0300

    Move upcase logic to rotate_character

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -2,12 +2,12 @@ module RotationalCipher
   CHARACTERS = %W{a b c d e f g h i j k l m n o p q r s t u v w x y z}
 
   def self.rotate(text, key)
-    text
-      .gsub(/[a-z]/) { |chr| rotate_character(chr, key) }
-      .gsub(/[A-Z]/) { |chr| rotate_character(chr.downcase, key).upcase }
+    text.gsub(/[a-zA-Z]/) { |chr| rotate_character(chr, key) }
   end
 
   def self.rotate_character(chr, key)
+    return CHARACTERS[(CHARACTERS.index(chr.downcase) + key) % CHARACTERS.length].upcase if chr == chr.upcase
+
     CHARACTERS[(CHARACTERS.index(chr) + key) % CHARACTERS.length]
   end
 end
