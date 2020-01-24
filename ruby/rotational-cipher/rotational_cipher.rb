fac4fe1979ea2e9041cc677374d88e15994123bb
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-01-24 14:19:33 -0300

    Rename characters to letters

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,13 +1,14 @@
 module RotationalCipher
-  CHARACTERS = %W{a b c d e f g h i j k l m n o p q r s t u v w x y z}
+  LETTERS = %W{a b c d e f g h i j k l m n o p q r s t u v w x y z}
+  LETTER = /[a-zA-Z]/
 
   def self.rotate(text, key)
-    text.gsub(/[a-zA-Z]/) { |chr| rotate_character(chr, key) }
+    text.gsub(LETTER) { |letter| rotate_letter(letter, key) }
   end
 
-  def self.rotate_character(chr, key)
-    return CHARACTERS[(CHARACTERS.index(chr.downcase) + key) % CHARACTERS.length].upcase if chr == chr.upcase
+  def self.rotate_letter(letter, key)
+    return rotate_letter(letter.downcase, key).upcase if letter == letter.upcase
 
-    CHARACTERS[(CHARACTERS.index(chr) + key) % CHARACTERS.length]
+    LETTERS[(LETTERS.index(letter) + key) % LETTERS.length]
   end
 end
