df3cec660a26ad02de44b61725894337a4e87e45
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-02-28 10:46:06 -0300

    Extract include_divisor? from Sieve#primes

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -5,11 +5,15 @@ class Sieve
 
   def primes
     (2..limit).each_with_object([]) do |number, calculed_primes|
-      calculed_primes << number unless calculed_primes.any? { |prime| (number % prime).zero? }
+      calculed_primes << number unless include_divisor?(calculed_primes, number)
     end
   end
 
   private
     attr_reader :limit
+  
+    def include_divisor?(calculed_primes, number)
+      calculed_primes.any? { |prime| (number % prime).zero? }
+    end    
 end
 
