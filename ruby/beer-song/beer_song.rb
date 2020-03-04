850742cdb007806b2521c47d86084b5523dbe250
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-04 16:14:31 -0300

    Refactory BeerSong.recite to avoid unecessary String#tap call

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,20 +1,26 @@
 module BeerSong
   def self.recite(bottles, takes)
-    "".tap do |lyris|
-      case bottles
-      when 2
-        lyris << "2 bottles of beer on the wall, 2 bottles of beer.\n"
-        lyris << "Take one down and pass it around, 1 bottle of beer on the wall.\n"  
-      when 1
-        lyris << "1 bottle of beer on the wall, 1 bottle of beer.\n"
-        lyris << "Take it down and pass it around, no more bottles of beer on the wall.\n"
-      when 0
-        lyris << "No more bottles of beer on the wall, no more bottles of beer.\n"
-        lyris << "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-      else
-        lyris << "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\n"
-        lyris << "Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.\n"  
-      end
+    case bottles
+    when 2
+      <<~TEXT
+      2 bottles of beer on the wall, 2 bottles of beer.
+      Take one down and pass it around, 1 bottle of beer on the wall.
+      TEXT
+    when 1
+      <<~TEXT
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.
+      TEXT
+    when 0
+      <<~TEXT
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.
+      TEXT
+    else
+      <<~TEXT
+      #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+      Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
+      TEXT
     end
   end
 end
