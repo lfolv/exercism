7cddf61c603a159cc09175daff359ef5db1065c2
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-05 15:29:23 -0300

    Extrac invalid function from equilateral()

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,5 +1,5 @@
 def equilateral(sides):
-    if any(side == 0 for side in sides):
+    if invalid(sides):
         return False
 
     return len(set(sides)) == 1
@@ -11,3 +11,7 @@ def isosceles(sides):
 
 def scalene(sides):
     pass
+
+
+def invalid(sides):
+    return any(side == 0 for side in sides)
