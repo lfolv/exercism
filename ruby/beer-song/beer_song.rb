e8795e765edb78ad8ecadcfad5eb80aeaf58a3f1
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-05 08:25:16 -0300

    Refactory entile BeerSong.recite
    This shold divide in more commits.

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,28 +1,35 @@
 module BeerSong
   def self.recite(bottles, takes)
-    bottles.downto(bottles - takes + 1).map do |n|
-      case n
-      when 2
-        <<~TEXT
-        2 bottles of beer on the wall, 2 bottles of beer.
-        Take one down and pass it around, 1 bottle of beer on the wall.
-        TEXT
-      when 1
-        <<~TEXT
-        1 bottle of beer on the wall, 1 bottle of beer.
-        Take it down and pass it around, no more bottles of beer on the wall.
-        TEXT
-      when 0
-        <<~TEXT
-        No more bottles of beer on the wall, no more bottles of beer.
-        Go to the store and buy some more, 99 bottles of beer on the wall.
-        TEXT
-      else
-        <<~TEXT
-        #{n} bottles of beer on the wall, #{n} bottles of beer.
-        Take one down and pass it around, #{n - 1} bottles of beer on the wall.
-        TEXT
-      end
-    end.join("\n")
+    limit = bottles - takes + 1
+
+    bottles
+      .downto(limit)
+      .map { |current_bottles| build_lyric(current_bottles) }
+      .join("\n")
+  end
+
+  def self.build_lyric(bottles)
+    case bottles
+    when 2
+      <<~TEXT
+      2 bottles of beer on the wall, 2 bottles of beer.
+      Take one down and pass it around, 1 bottle of beer on the wall.
+      TEXT
+    when 1
+      <<~TEXT
+      1 bottle of beer on the wall, 1 bottle of beer.
+      Take it down and pass it around, no more bottles of beer on the wall.
+      TEXT
+    when 0
+      <<~TEXT
+      No more bottles of beer on the wall, no more bottles of beer.
+      Go to the store and buy some more, 99 bottles of beer on the wall.
+      TEXT
+    else
+      <<~TEXT
+      #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
+      Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
+      TEXT
+    end
   end
 end
