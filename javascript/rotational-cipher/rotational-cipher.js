b3ce3c5ba7a9a23a1a3af68872ef3cc2bf12c431
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-10 08:26:07 -0300

    Extract rotateLetter() from RotationalCipher.rotate()

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -2,8 +2,13 @@ const ALPH = [...'abcdefghijklmnopqrstuvwxyz']
 
 export class RotationalCipher {
   static rotate(input, rotate) {
+    return input.replace(/[a-z]/g, (letter) =>
+      RotationalCipher.rotateLetter(letter, rotate))
+  }
+
+  static rotateLetter(letter, rotate) {
     return ALPH[
-      (ALPH.indexOf(input) + rotate) % ALPH.length
+      (ALPH.indexOf(letter) + rotate) % ALPH.length
     ]
   }
 }
