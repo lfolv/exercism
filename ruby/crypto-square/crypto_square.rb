5fa0e9516bc3fe8dc89e1080dbc35b6790615974
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-30 15:49:33 -0300

    Create Crypto.normalize function
    * Downcase all characters to use only avaliable characters
    * Remove all spaces to do the square.
    * Ignore punctuation since this crypto don't work with this
    characters

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,7 +1,11 @@
 class Crypto
   attr_reader :ciphertext
 
+  def self.normalize(plaintext)
+    plaintext.downcase.gsub(/\W*/, '')
+  end
+
   def initialize(plaintext)
-    @ciphertext = plaintext
+    @ciphertext = Crypto.normalize(plaintext)
   end
 end
