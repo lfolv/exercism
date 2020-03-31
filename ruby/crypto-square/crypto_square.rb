0a32b303597e547fe5cd158f34c4605e11d7f8d2
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-03-30 21:02:23 -0300

    Create Crypto#ciphertext method

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,11 +1,29 @@
 class Crypto
-  attr_reader :ciphertext
-
-  def self.normalize(plaintext)
-    plaintext.downcase.gsub(/\W*/, '')
-  end
-
   def initialize(plaintext)
-    @ciphertext = Crypto.normalize(plaintext)
+    @plaintext = plaintext
   end
+
+  def plaintext
+    @normalized_plain_text ||= @plaintext.downcase.gsub(/\W*/, '')
+  end
+
+  def ciphertext
+    until plaintext.size % column_size == 0
+      plaintext << ' '
+    end
+    
+    plaintext
+      .each_char
+      .each_slice(column_size)
+      .to_a
+      .transpose
+      .map(&:join)
+      .join(' ')
+  end
+  
+  private
+    def column_size
+      size = Math.sqrt(plaintext.length).ceil
+      size.zero? ? 1 : size
+    end
 end
--- ruby/crypto-square/crypto_square_test.rb
+++ ruby/crypto-square/crypto_square_test.rb
@@ -4,7 +4,6 @@ require_relative 'crypto_square'
 # Common test data version: 3.2.0 e00dfb3
 class CryptoSquareTest < Minitest::Test
   def test_empty_plaintext_results_in_an_empty_ciphertext
-    # skip
     plaintext = ''
     assert_equal "", Crypto.new(plaintext).ciphertext
   end
@@ -25,19 +24,16 @@ class CryptoSquareTest < Minitest::Test
   end
 
   def test_9_character_plaintext_results_in_3_chunks_of_3_characters
-    skip
     plaintext = 'This is fun!'
     assert_equal "tsf hiu isn", Crypto.new(plaintext).ciphertext
   end
 
   def test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space
-    skip
     plaintext = 'Chill out.'
     assert_equal "clu hlt io ", Crypto.new(plaintext).ciphertext
   end
 
   def test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces
-    skip
     plaintext = 'If man was meant to stay on the ground, god would have given us roots.'
     assert_equal "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn  sseoau ", Crypto.new(plaintext).ciphertext
   end
