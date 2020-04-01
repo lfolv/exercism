f79ae324b86e3e7b5506c598695d4d1ed103bb3a
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-04-01 14:52:32 -0300

    Create rna_transcription module

---
+++ python/rna-transcription/rna_transcription_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+from rna_transcription import to_rna
+
+
+# Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
+
+class RnaTranscriptionTest(unittest.TestCase):
+
+    def test_empty_rna_sequence(self):
+        self.assertEqual(to_rna(""), "")
+
+    @unittest.skip
+    def test_transcribes_cytosine_to_guanine(self):
+        self.assertEqual(to_rna('C'), 'G')
+
+    @unittest.skip
+    def test_transcribes_guanine_to_cytosine(self):
+        self.assertEqual(to_rna('G'), 'C')
+
+    @unittest.skip
+    def test_transcribes_thymine_to_adenine(self):
+        self.assertEqual(to_rna('T'), 'A')
+
+    @unittest.skip
+    def test_transcribes_adenine_to_uracil(self):
+        self.assertEqual(to_rna('A'), 'U')
+
+    @unittest.skip
+    def test_transcribes_all_occurrences(self):
+        self.assertEqual(to_rna('ACGTGGTCTTAA'), 'UGCACCAGAAUU')
+
+
+if __name__ == '__main__':
+    unittest.main()
