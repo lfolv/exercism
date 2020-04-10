9cb9649fd01a3e5d2829571f5567b866814d8783
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-04-10 18:31:04 -0300

    Refactory Squares class
    * Extract sum() to Array prototype to use as a pipe function

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,35 +1,33 @@
-function range(start: number, end: number, step = 1) {
-  const result = [];
-
-  for (let i = start; i <= end; i += step) {
-    result.push(i);
-  }
-
-  return result;
-}
-
-function sum(array: number[]) {
-  return array.reduce((acc, value) => acc + value, 0);
-}
-
 class Squares {
   constructor(private n: number) {}
 
   get squareOfSum() {
-    const values = range(0, this.n);
-
-    return sum(values) ** 2;
+    return [...range(0, this.n)].sum() ** 2;
   }
 
   get sumOfSquares() {
-    const values = range(0, this.n).map((value) => value ** 2);
-
-    return sum(values);
+    return [...range(0, this.n)].map((value) => value ** 2).sum();
   }
 
   get difference() {
     return this.squareOfSum - this.sumOfSquares;
   }
 }
+
+function* range(start: number, end: number, step = 1) {
+  for (let i = start; i <= end; i += step) {
+    yield i;
+  }
+}
+
+declare global {
+  interface Array<T> {
+    sum(): number;
+  }
+}
+
+Array.prototype.sum = function <T>(): T {
+  return this.reduce((acc, value) => acc + value, 0);
+};
 
 export default Squares;
