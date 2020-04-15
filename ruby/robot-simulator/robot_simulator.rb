03a02af4bf1a302a528e83388a0437829680d4e3
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-04-15 19:12:56 -0300

    Resolve Robot Simulator from Ruby track
    close #25

---
+++ ruby/robot-simulator/robot_simulator.rb
@@ -0,0 +1,82 @@
+class Robot
+  DIRECTIONS = [:east, :west, :north, :south]
+
+  attr_reader :bearing 
+  attr_accessor :coordinates
+  
+  def turn_right
+    case bearing
+    when :north
+      orient :east
+    when :east
+      orient :south
+    when :south
+      orient :west
+    when :west
+      orient :north
+    end
+  end
+
+  def turn_left
+    case bearing
+    when :north
+      orient :west
+    when :east
+      orient :north
+    when :south
+      orient :east
+    when :west
+      orient :south
+    end
+  end
+
+  def orient(direction)
+    raise ArgumentError unless DIRECTIONS.include?(direction)
+
+    @bearing = direction
+  end
+
+  def at(x, y)
+    @coordinates = [x, y]
+  end
+
+  def advance
+    x, y = @coordinates
+
+    case bearing
+    when :north
+      @coordinates = [x, y + 1]
+    when :east
+      @coordinates = [x + 1, y]
+    when :south
+      @coordinates = [x, y - 1]
+    when :west
+      @coordinates = [x - 1, y]
+    end
+  end
+end
+
+class Simulator
+  INSTRUCTIONS_TO_METHOD = {
+    'L' => :turn_left,
+    'R' => :turn_right,
+    'A' => :advance
+  }
+
+  def place(robot, x:, y:, direction:)
+    robot.coordinates = [x, y]
+    robot.orient direction
+  end
+
+  def evaluate(robot, serie_of_instructions)
+    instructions(serie_of_instructions).each do |method_name|
+      robot.method(method_name).call
+    end
+  end
+
+  def instructions(serie_of_instructions)
+    serie_of_instructions
+      .each_char
+      .map(&INSTRUCTIONS_TO_METHOD)
+  end
+end
