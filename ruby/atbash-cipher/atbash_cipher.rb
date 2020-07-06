bcb80d091e8283055c5a2f53a8cf1a2708310e42
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-07-06 20:48:24 -0300

    fixup! Update Atbash.encode to remove spaces

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,11 +1,25 @@
+class String
+  def remove_no_word_characters
+    self.gsub(/[\s,.]/, '')
+  end
+
+  def split_in_groups_of(length)
+    self
+      .each_char
+      .each_slice(5)
+      .map { |chrs| chrs.join('') }
+  end
+end
+
 module Atbash
+  LENGTH = 5
+
   def self.encode(message)
-    result = ""
-
-    message.gsub(/\s/, '').downcase.each_char do |chr|
-      result << ('a'.ord + 'z'.ord - chr.ord).chr
-    end
-
-    result
+    message
+      .downcase
+      .remove_no_word_characters
+      .gsub(/[a-z]/) { |chr| ('a'.ord + 'z'.ord - chr.ord).chr }
+      .split_in_groups_of(LENGTH)
+      .join(' ')
   end
 end
--- ruby/atbash-cipher/atbash_cipher_test.rb
+++ ruby/atbash-cipher/atbash_cipher_test.rb
@@ -19,23 +19,19 @@ class AtbashTest < Minitest::Test
   end
 
   def test_encode_long_word
-    skip
     assert_equal 'nrmwy oldrm tob', Atbash.encode('mindblowingly')
   end
 
   def test_encode_numbers
-    skip
     assert_equal('gvhgr mt123 gvhgr mt',
                  Atbash.encode('Testing, 1 2 3, testing.'))
   end
 
   def test_encode_sentence
-    skip
     assert_equal 'gifgs rhurx grlm', Atbash.encode('Truth is fiction.')
   end
 
   def test_encode_all_the_things
-    skip
     plaintext = 'The quick brown fox jumps over the lazy dog.'
     cipher = 'gsvjf rxpyi ldmul cqfnk hlevi gsvoz abwlt'
     assert_equal cipher, Atbash.encode(plaintext)
