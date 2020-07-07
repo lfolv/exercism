abc549dfff64948bd68faaa8d50a5f9885ae7af7
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-07-07 08:13:08 -0300

    Create ISBN class with isValid method

---
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -0,0 +1,27 @@
+export default class ISBN {
+  private identification: string
+
+  constructor(identification: string) {
+    this.identification = identification.replace(/-/g, '')
+  }
+
+  isValid() {
+    if (this.identification.length !== 10) {
+      return false
+    }
+
+    let sum = 0
+
+    for (let i = 0; i < 9; i++) {
+      sum += Number(this.identification[i]) * (10 - i)
+    }
+
+    if (this.identification[9] === 'X') {
+      sum += 10
+    } else {
+      sum += Number(this.identification[9])
+    }
+
+    return sum % 11 === 0
+  }
+}
