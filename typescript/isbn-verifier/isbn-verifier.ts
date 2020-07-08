69cba1e51426e01df5921fd0c2bb91c7fa9dd4e1
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-07-08 08:15:13 -0300

    Extract constants from ISBN class

--- typescript/isbn-verifier/isbn-verifier.ts
+++ typescript/isbn-verifier/isbn-verifier.ts
@@ -1,31 +1,38 @@
+const DIVISOR = /-/g
+const LENGTH = 10
+const LAST_DIGIT_INDEX = 9
+const X_VALUE = 10
+
 export default class ISBN {
   private identification: string
 
   constructor(identification: string) {
-    this.identification = identification.replace(/-/g, '')
+    this.identification = identification.replace(DIVISOR, '')
   }
 
   isValid() {
-    if (this.identification.length !== 10) {
+    if (this.identification.length !== LENGTH) {
       return false
     }
 
     return this.sum() % 11 === 0
   }
 
-  sum() {
+  private sum() {
     let sum = 0
 
-    for (let i = 0; i < 9; i++) {
-      sum += Number(this.identification[i]) * (10 - i)
-    }
-
-    if (this.identification[9] === 'X') {
-      sum += 10
-    } else {
-      sum += Number(this.identification[9])
+    for (let i = 0; i < LENGTH; i++) {
+      sum += this.valueFromIndex(i)
     }
 
     return sum
+  }
+
+  private valueFromIndex(index: number) {
+    if (index === LAST_DIGIT_INDEX && this.identification[LAST_DIGIT_INDEX] === 'X') {
+      return X_VALUE
+    }
+
+    return Number(this.identification[index]) * (LENGTH - index)
   }
 }
