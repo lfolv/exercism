4098ea98fa65fbf1e1c368a03792f90e5f9b00c5
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-07-13 20:11:21 -0300

    Create Matrix#saddle_points

--- ruby/saddle-points/saddle_points.rb
+++ ruby/saddle-points/saddle_points.rb
@@ -18,10 +18,23 @@ class Matrix
   end
 
   def saddle_points
-    []
+    result = []
+
+    rows.each.with_index do |values, row_index|
+      values.each.with_index do |_, column_index|
+        result << [row_index, column_index] if is_saddle_point(row_index, column_index)
+      end
+    end
+
+    result
   end
 
   private
 
   attr_reader :matrix
+
+  def is_saddle_point(row_index, column_index)
+    value = rows[row_index][column_index]
+    rows[row_index].max == value && columns[column_index].min == value
+  end
 end
