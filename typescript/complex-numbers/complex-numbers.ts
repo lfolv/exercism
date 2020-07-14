827c0d6caf4433f3fcc2ad15990eba2143668de1
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2020-07-14 18:23:23 -0300

    Add add method to ComplexNumber

--- typescript/complex-numbers/complex-numbers.ts
+++ typescript/complex-numbers/complex-numbers.ts
@@ -1,5 +1,12 @@
 class ComplexNumber {
   constructor(public real: number, public imag: number) {}
+
+  add(other: ComplexNumber) {
+    const real = this.real + other.real
+    const imag = this.imag + other.imag
+
+    return new ComplexNumber(real, imag)
+  }
 }
 
 export default ComplexNumber
